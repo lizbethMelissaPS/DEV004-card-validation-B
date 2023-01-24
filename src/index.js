fa556168c275405d5c263120f2be3042983682cc
lizbethMelissaPS <melissasud27@gmail.com>
2023-01-24 03:13:25 -0500

    vista simple en html y metodos implementado en el obj

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+/* const soloNumero = document.getElementById('idtarjeta');
+const valortecla = this.value.keyCode;
+console.log("valortecla: " + valortecla);
+soloNumero.addEventListener("onkeypress", function (event) {
+    console.log("soloNumero: " + soloNumero);
+    return (event.keyCode >= 48) && (event.keyCode <= 57)
+    
+})
+ */
+const numero = document.getElementById('idtarjeta');
+console.log("numero: " + numero);
+/*onkeyup HTML event: evento que se dispara cuando se "suelta" una tecla,
+ momento donde te interesa obtener el valor del input.
+DOM addEventListener: posibilidad de bindear eventos a elementos del DOM
+*/
+numero.addEventListener("keyup", function () {
+    const valor = this.value;
+    console.log("numero: " + valor);
+    validator.isValid(valor);
+    validator.maskify(valor);
+})
+//invocar
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,48 @@
 const validator = {
   // ...
-};
+  isValid: function (cardNumber) {
+    const reverseNumber = cardNumber.split('').reverse();
+    console.log('reverseNumber: ' + reverseNumber);
+    //dobla segundo digito par
+    const doblarDigito = [];
+    for (let i = 0; i < reverseNumber.length; i++) {
+      //utilizando operador ternario
+      (i % 2 === 1) ? doblarDigito.push(reverseNumber[i] * 2) : doblarDigito.push(reverseNumber[i]);
+    }
+    console.log('doblarDigito: ' + doblarDigito);
+    //suma de digito
+    const sumarDigitos = []; 
+    for (let i = 0; i < doblarDigito.length; i++) {
+      if (doblarDigito[i] > 9) {
+        const arrayDigito = doblarDigito[i].toString().split('');
+        let suma = 0;
+        for (let j = 0; j < arrayDigito.length; j++) {
+          suma += parseInt(arrayDigito[j]);
+        }
+        sumarDigitos.push(suma);
+      } else {
+        sumarDigitos.push(doblarDigito[i]);
+      }
+    }
+    console.log('sumarDigitos: ' + sumarDigitos);
+    //suma final
+    const sumaFinal=sumarDigitos.reduce((a, b) => a + b);
+    console.log('sumaFinal: ' + sumaFinal);
+    return (sumaFinal % 10 === 0) ? true : false;
+    
+  },
+  maskify: function (cardNumber) {
+    console.log('cardNumber: '+ typeof cardNumber + cardNumber);
+    //reemplazar
+    const str = cardNumber.slice(0, -4);
+    console.log('str: '+ str);
+    const strRemplazado=cardNumber.replace(str,'*');
+    console.log('strRemplazado: '+ strRemplazado);
+    
+  }
+}
+/*
+validator.isValid(creditCardNumber)
+validator.maskify(creditCardNumber)*/
 
 export default validator;
