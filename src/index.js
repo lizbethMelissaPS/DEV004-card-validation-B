9b60a866f131988a0d07d3312f7531ea232e70c0
lizbethMelissaPS <melissasud27@gmail.com>
2023-01-26 03:18:23 -0500

    validaciones para el input 👌

--- src/index.js
+++ src/index.js
@@ -2,20 +2,24 @@ import validator from './validator.js';
 
 //console.log(validator);
 //keydown  submit input  keyup  4083952015263
-const element = document.getElementById("btnEnviar") ;
+const element = document.getElementById("btnEnviar");
 element.addEventListener("click", validate);
 
+
+
 function validate() {
-  //validator.isValid(numero); 
   const numero = document.getElementById('idtarjeta').value;
-  console.log("numeroA: " + numero); 
-  const valid = validator.isValid(numero);
-  console.log("valid: " + valid);
-  if (valid) {
-    document.getElementById("result").innerHTML = "El numero de tarjeta " + validator.maskify(numero)+"  es validad!"
-  }
-  else {
-    document.getElementById("result").innerHTML = "Numero de tarjeta  invalidad";
+  const soloNumeros = document.getElementById("result").alert = /[0-9]+/.test(numero) ? true : alert('Ingrese solo numeros');
+  if (soloNumeros) {
+    const valid = validator.isValid(numero);
+    if (valid) {
+      document.getElementById("result").innerHTML = "El numero de tarjeta " + validator.maskify(numero) + "  es validad!"
+    }
+    else {
+      document.getElementById("result").innerHTML = "Numero de tarjeta  invalidad";
+    }
+  } else {
+    alert("Ingresar numero de tarjeta");
   }
 }
 
